2795e926b8c4ba0eae7a7454c506974199eae2db
Jake Overall <joverall22@gmail.com>
2018-12-04 18:04:59 -0700

    dun dun, du du dun, dun dun dunnn, dun du du dun du du dunn - magic

---
+++ src/components/Student.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="student col-3 card">
+    <h2>{{s.name}}</h2>
+    <button @click="addPoints">add points</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'student',
+    props: ['s'],
+    data() {
+      return {
+
+      }
+    },
+    computed: {},
+    methods: {
+      addPoints() {
+        this.$store.dispatch('addPoints', this.s.house)
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <div class="row mb-5 pb-5">
+      <div class="col">
+        <img class="left" alt="Vue logo" src="../assets/logo.png">
+        <img class="right" alt="Vue logo" src="../assets/logo.png">
+        <h2 class="title">itchcraft and Wizardry</h2>
+      </div>
+    </div>
+    <houses class="mt-5 pt-5"></houses>
+    <div class="row">
+      <student v-for="student in students" :key="student._id" :s="student"></student>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Student from '@/components/Student.vue'
+  import Houses from '@/components/Houses.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    //same as running a method on instantiation of constructor in controller or service with vanilla
+    mounted() {
+      this.$store.dispatch('getStudents')
+    },
+    computed: {
+      hufflepuff() {
+        return this.$store.state.students.filter(s => s.house == 'Hufflepuff')
+      },
+      students() {
+        return this.$store.state.students.filter(c => c.role == 'student')
+      }
+    },
+    components: {
+      Student,
+      Houses
+    }
   }
-}
 </script>
+
+<style>
+  .home {
+    position: relative;
+  }
+
+  .left {
+    transform: rotate(350deg);
+    position: absolute;
+    top: -3vh;
+  }
+
+  .right {
+    transform: rotate(10deg);
+    position: absolute;
+    top: -3vh;
+    left: 7vw;
+  }
+
+  .title {
+    position: absolute;
+    left: 18vw;
+    top: 12vh;
+  }
+</style>
